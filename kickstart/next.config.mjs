6d3d7cd2ef74a31000ecb55a7089588e2e1f5c52
farid <farid.moradi9@yahoo.com>
2024-06-06 09:34:05 +0330

    change env

--- kickstart/next.config.mjs
+++ kickstart/next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  swcMinify: false,
-  compiler: {
-    removeConsole: false,
-    styledComponents: true,
-  },
-  optimizeFonts: false,
 };
 
 export default nextConfig;
